84bdc296dc7f5214dbbfbd14ba2db5c9181ba6a8
jirigajdacz <146259868+jirigajdacz@users.noreply.github.com>
2023-11-08 09:18:37 +0100

    Add files via upload

---
+++ ukol 6.py
@@ -0,0 +1,34 @@
+class Auto:
+    def __init__(self, registracni_znacka, typ_vozidla, najete_km):
+        self.registracni_znacka = registracni_znacka
+        self.typ_vozidla = typ_vozidla
+        self.najete_km = najete_km
+        self.dostupne = True  
+
+    def pujc_auto(self):
+        if self.dostupne:
+            self.dostupne = False
+            return "Potvrzuji zapůjčení vozidla"
+        else:
+            return "Vozidlo není k dispozici"
+
+    def get_info(self):
+        return f"Registrační značka: {self.registracni_znacka}, Značka a typ vozidla: {self.typ_vozidla}"
+
+
+auto1 = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
+auto2 = Auto("1P3 4747", "Škoda Octavia", 41253)
+
+
+vyber = input("Jakou značku si přejete půjčit (Peugeot nebo Škoda)? ").strip().lower()
+
+if vyber == "peugeot":
+    print(auto1.get_info())
+    vysledek = auto1.pujc_auto()
+elif vyber == "škoda":
+    print(auto2.get_info())
+    vysledek = auto2.pujc_auto()
+else:
+    vysledek = "Neplatný výběr značky."
+
+print(vysledek)
